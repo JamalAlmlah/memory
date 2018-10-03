ebbedab364ffc6b9ec8ccb8288742f4d1a82fea5
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-10-03 10:57:23 +0200

    La till memory bricka

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,3 @@
-/* TODO: Ett fungerande memory-spel ska skapas som använder sig utav de bilder som finns ibland filerna. */
-// TODO: Spelet ska ha brickor uppdelade i 4 rader, med 4 brickor på varje rad. */
-// TODO: Spelaren ska kunna se hur många gånger den försökt hitta par.*/
-// TODO: Spelaren ska kunna se hur många par den har.*/
-// TODO: Spelar ska kunna se under hur många sekunder som han har spelat. */
-// TODO: När ett par hittas så ska brickorna försvinna.*/
-// TODO: Det ska enkelt gå att ladda in flera spel, genom att anropa en funktion flera gånger. */
-// TODO: När spelet är slut ska sekunder sluta räknas*/
-// TODO: En enklare dokumentation i README.md som ska vara skriven i markup språket Markdown. Bör innehålla kortare information om vad som ligger i respektive fil samt vilka kommandon som ska köras för att starta utvecklingsserver samt hur man bygger en build.*/
+/* eslint-disable no-unused-vars */
+import style from '../css/style.css';
+/* eslint-enable no-unused-vars */
