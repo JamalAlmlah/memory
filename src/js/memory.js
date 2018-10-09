b008a6802548ad2015c60dd5259273ddfb482bfd
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-10-09 14:55:17 +0200

    Flyttade till egen funktion

--- src/js/memory.js
+++ src/js/memory.js
@@ -1,4 +1,3 @@
-// TODO: Spelet ska ha brickor uppdelade i 4 rader, med 4 brickor på varje rad. */
 // TODO: Spelaren ska kunna se hur många gånger den försökt hitta par.*/
 // TODO: Spelaren ska kunna se hur många par den har.*/
 // TODO: Spelar ska kunna se under hur många sekunder som han har spelat. */
@@ -6,22 +5,42 @@
 // TODO: Det ska enkelt gå att ladda in flera spel, genom att anropa en funktion flera gånger. */
 // TODO: När spelet är slut ska sekunder sluta räknas*/
 // TODO: En enklare dokumentation i README.md som ska vara skriven i markup språket Markdown. Bör innehålla kortare information om vad som ligger i respektive fil samt vilka kommandon som ska köras för att starta utvecklingsserver samt hur man bygger en build.*/
+const renderMemory = () => {
+
+
+};
 
 const memory = () => {
-  const rows = 4;
-  const columns = 4;
+  const renderOptions = {
+    rows: 4,
+    columns: 4
+  };
 
+  const bricks = {
+    tiles: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+  };
   const containerId = 'memory';
-
-  const container = document.getElementById(containerId);
+  renderMemory(containerId, bricks);
   const template = document.querySelector('#memory template');
   const templateDiv = template.content.firstElementChild;
   const div = document.importNode(templateDiv, false);
   container.appendChild(div);
-
-  for (let i = 0; i < rows * columns; i++) {
+  for (let i = 0; i < bricks.tiles.length; i++) {
+    const handleClick = event => {
+      let img;
+      if (event.target.tagName === 'DIV') {
+        img = event.target.firstElementChild;
+      } else {
+        img = event.target;
+      }
+      const path = `images/${bricks.tiles[i]}.png`;
+      img.setAttribute('src', path);
+    };
     const brick = document.importNode(templateDiv.firstElementChild, true);
+    brick.addEventListener('click', handleClick);
     div.appendChild(brick);
+    const container = document.getElementById(containerId);
+
   }
 };
 export default memory;
