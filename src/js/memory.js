5fc1302aa42448e3a35df0370dfead98a2fa5898
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-10-10 09:56:21 +0200

    Lagt till spel logik

--- src/js/memory.js
+++ src/js/memory.js
@@ -5,9 +5,60 @@
 // TODO: Det ska enkelt gå att ladda in flera spel, genom att anropa en funktion flera gånger. */
 // TODO: När spelet är slut ska sekunder sluta räknas*/
 // TODO: En enklare dokumentation i README.md som ska vara skriven i markup språket Markdown. Bör innehålla kortare information om vad som ligger i respektive fil samt vilka kommandon som ska köras för att starta utvecklingsserver samt hur man bygger en build.*/
-const renderMemory = () => {
+const turnBrick = (bricks, img) => {
+  if (bricks.first === null) {
+    bricks.first = img;
+  } else {
+    bricks.second = img;
 
+    if (bricks.first.getAttribute('src') === bricks.second.getAttribute('src')) {
+      const removeBrick = () => {
+        bricks.first.parentElement.classList.add('hidden');
+        bricks.second.parentElement.classList.add('hidden');
 
+        bricks.first = null;
+        bricks.second = null;
+      };
+      window.setTimeout(removeBrick, 500);
+    } else {
+      const turnBackBrick = () => {
+        const path = 'images/0.png';
+        bricks.first.setAttribute('src', path);
+        bricks.second.setAttribute('src', path);
+        bricks.first = null;
+        bricks.second = null;
+      };
+      window.setTimeout(turnBackBrick, 500);
+    }
+  }
+};
+
+const renderMemory = (containerId, bricks) => {
+  const container = document.getElementById(containerId);
+  const template = document.querySelector('#memory template');
+  const templateDiv = template.content.firstElementChild;
+  const div = document.importNode(templateDiv, false);
+  container.appendChild(div);
+
+  for (let i = 0; i < bricks.tiles.length; i++) {
+    const handleClick = event => {
+      let img;
+      if (event.target.tagName === 'DIV') {
+        img = event.target.firstElementChild;
+      } else {
+        img = event.target;
+      }
+
+      const path = `images/${bricks.tiles[i]}.png`;
+      img.setAttribute('src', path);
+
+      turnBrick(bricks, img);
+    };
+
+    const brick = document.importNode(templateDiv.firstElementChild, true);
+    brick.addEventListener('click', handleClick);
+    div.appendChild(brick);
+  }
 };
 
 const memory = () => {
@@ -17,30 +68,11 @@ const memory = () => {
   };
 
   const bricks = {
+    first: null,
+    second: null,
     tiles: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
   };
   const containerId = 'memory';
   renderMemory(containerId, bricks);
-  const template = document.querySelector('#memory template');
-  const templateDiv = template.content.firstElementChild;
-  const div = document.importNode(templateDiv, false);
-  container.appendChild(div);
-  for (let i = 0; i < bricks.tiles.length; i++) {
-    const handleClick = event => {
-      let img;
-      if (event.target.tagName === 'DIV') {
-        img = event.target.firstElementChild;
-      } else {
-        img = event.target;
-      }
-      const path = `images/${bricks.tiles[i]}.png`;
-      img.setAttribute('src', path);
-    };
-    const brick = document.importNode(templateDiv.firstElementChild, true);
-    brick.addEventListener('click', handleClick);
-    div.appendChild(brick);
-    const container = document.getElementById(containerId);
-
-  }
 };
 export default memory;
